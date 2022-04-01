58d2566e596d1a1d57c7bea16f4bef134817de68
knikvibrik <101859189+knikvibrik@users.noreply.github.com>
2022-04-01 10:37:55 +0300

    lesson4

--- lesson2-game.py
+++ lesson2-game.py
@@ -1,3 +1,20 @@
+import random
+
+def try_next_move():
+    if random.random() < 0.25:
+        print("Try to go right")
+
+    elif random.random() < 0.5:
+        print("Try to go left")
+
+    elif random.random() < 0.75:
+        print("Try to go bottom")
+
+    elif random.random() < 0.1:
+        print("Try to go top")
+
+try_next_move()
+
 map1 = [
     [12, 0, 1, 0],
     [1, 0, 1, 0],
@@ -10,7 +27,27 @@ map2 = [
     [1, 0, 0, 0],
     [0, 0, 0, 24]
 ]
-map = map2
+map3 = [
+    [12, 1, 0, 0],
+    [0, 0, 1, 0],
+    [1, 0, 1, 0],
+    [24, 0, 0, 0]
+]
+map4 = [
+    [12, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0], 
+    [1, 0, 0, 0, 0, 24]
+]
+map5 = [
+    [0, 1, 0, 0, 0, 0],
+    [0, 12, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0],
+    [1, 0, 1, 24, 0, 0], 
+    [1, 0, 0, 0, 0, 0]
+]
+map = map5
 
 # define starting position
 start_position_x = 0
@@ -21,18 +58,46 @@ def move(step, current_position_x,current_position_y):
     print("Current step is: ", step)
     print("Current current_position_y = ", current_position_y)
     print("Current current_position_x = ", current_position_x)
-    can_move_right = current_position_x <= 2 and map[current_position_y][current_position_x+1] == 0
-    can_move_to_bottom =current_position_y <= 2 and map[current_position_y+1][current_position_x] == 0
-   
+
+    can_move_right = current_position_x <= 4 and map[current_position_y][current_position_x + 1] == 0
+    can_move_to_bottom = current_position_y <= 3 and map[current_position_y + 1][current_position_x] == 0
+    can_move_left = current_position_x <= 4 and map[current_position_y][current_position_x - 1] == 0
+    can_move_up = current_position_x <= 4 and map[current_position_y - 1][current_position_x] == 0 
+
+    right_is_finish = current_position_x <= 4 and map[current_position_y][current_position_x + 1] == 24
+    bottom_is_finish = current_position_y <= 3 and map[current_position_y + 1][current_position_x] == 24
+    left_is_finish = current_position_x <= 4 and map[current_position_y][current_position_x - 1] == 24 
+    up_is_finish = current_position_x <= 4 and map[current_position_y - 1][current_position_x] == 24 
+
+
+    if right_is_finish:
+        print("Found finish on right")
+        return False
+    if bottom_is_finish:
+        print("Found finish on bottom")
+        return False
+    if left_is_finish:
+        print("Found finish on left")
+        return False
+    if up_is_finish:
+        print("Found finish on left")
+        return False
+    
     if can_move_right:
         print("Should move right")
         current_position_x = current_position_x + 1
     if can_move_to_bottom:
         print("Should move down")
         current_position_y = current_position_y + 1
+    if can_move_left:
+        print("Should move left")
+        current_position_x = current_position_x - 1
+    if can_move_up:
+        print("Should move up")
+        current_position_y = current_position_y - 1
 
-    return [current_position_x, current_position_y]
-
-new_position = move(1, start_position_x, start_position_y)
-new_position = move(2, new_position[0], new_position[1])
-new_position = move(3, new_position[0], new_position[1])
+current_move_number = 1
+new_position = move(current_move_number, start_position_x, start_position_y)
+while new_position:
+    current_move_number = current_move_number + 1
+    new_position = move(current_move_number, new_position[0], new_position[1])
